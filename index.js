8dab7da52e1ef4d99302a7b998a49370b277ffe6
silenceismy <wesleytan617@gmail.com>
2022-07-22 02:44:21 +0800

    changes

--- index.js
+++ index.js
@@ -19,37 +19,37 @@ document.addEventListener("keypress", function (event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      var tom1 = new Audio("sounds/Piano-C.wav");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
+      var tom2 = new Audio("sounds/Piano-D.wav");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
+      var tom3 = new Audio("sounds/Piano-E.wav");
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3");
+      var tom4 = new Audio("sounds/Piano-F.wav");
       tom4.play();
       break;
 
     case "j":
-      var snare = new Audio("sounds/snare.mp3");
+      var snare = new Audio("sounds/Piano-G.wav");
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio("sounds/crash.mp3");
+      var crash = new Audio("sounds/Piano-A.wav");
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio("sounds/kick-bass.mp3");
+      var kick = new Audio("sounds/Piano-B.wav");
       kick.play();
       break;
 
